32076025834166b5ff4c3f78f9bc15e2fa2cfc20
Alexander <as.frantsev@gmail.com>
2023-12-22 18:13:25 +0300

    update dependences

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import gradio as gr
 from transformers import pipeline
-from pydantic import BaseModel
+from loguru import logger
+# from pydantic import BaseModel
 
 # RU_SUMMARY_MODEL = "IlyaGusev/rubart-large-sum"
 RU_SUMMARY_MODEL = "IlyaGusev/mbart_ru_sum_gazeta"
@@ -9,18 +10,24 @@ RU_SENTIMENT_MODEL = "seara/rubert-tiny2-russian-sentiment"
 EN_SUMMARY_MODEL = "sshleifer/distilbart-cnn-12-6"
 EN_SENTIMENT_MODEL = "ProsusAI/finbert"
 
-class Summarizer(BaseModel):
+class Summarizer():
     ru_summary_pipe: pipeline
     ru_sentiment_pipe: pipeline
     
     def __init__(self) -> None:
-        self.ru_summary_pipe = pipeline("summarization", model=RU_SUMMARY_MODEL)
-        self.ru_summary_pipe = pipeline("sentiment-analysis", model=RU_SENTIMENT_MODEL)
+        self.ru_summary_pipe = pipeline("summarization", model=RU_SUMMARY_MODEL, max_length=100, truncation=True)
+        self.ru_sentiment_pipe = pipeline("sentiment-analysis", model=RU_SENTIMENT_MODEL)
         
     def summarize(self, text: str) -> str:
         result = {}
-        result["summary"] = self.ru_summary_pipe(text)[0]["label"]
-        result["sentiment"] = self.ru_sentiment_pipe(text)[0]["label"]
+        response_summary = self.ru_summary_pipe(text)
+        logger.info(response_summary)
+        result["summary"] = response_summary[0]["summary_text"]
+        
+        response_sentiment = self.ru_sentiment_pipe(text)
+        logger.info(response_sentiment)
+        result["sentiment"] = response_sentiment[0]["label"]
+        
         return f"Summary: {result['summary']}\n Sentiment:{result['sentiment']}"
 
 pipe = Summarizer()
