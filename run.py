47aa4c11bd6801497395bd3ee205a2459037480a
Alexander <as.frantsev@gmail.com>
2024-06-16 23:16:12 +0300

    add local ssl for dev

--- run.py
+++ run.py
@@ -1,4 +1,19 @@
 import uvicorn
+import ssl
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain(
+    "/home/alexander/certs/cert.pem", keyfile="/home/alexander/certs/key.pem"
+)
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run(
+        app="main:app",
+        host="localhost",
+        port=8000,
+        reload=True,
+        proxy_headers=True,
+        forwarded_allow_ips="*",
+        ssl_certfile="/home/alexander/certs/cert.pem",
+        ssl_keyfile="/home/alexander/certs/key.pem",
+    )
